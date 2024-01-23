d99cfcec18114aed3548611685c879f9870665dd
Felipe alexandre <lipe.alx@gmail.com>
2024-01-23 12:54:19 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Teste from './Teste'
+import EstiloGlobal from './styles'
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
